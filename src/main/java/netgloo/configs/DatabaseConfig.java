e8458a7fb6fed75ab0fb5747de7c8111e5901c2e
Star Mercy <abhimittal19@gmail.com>
2017-05-15 18:21:46 +0530

    sadsa

--- src/main/java/netgloo/configs/DatabaseConfig.java
+++ src/main/java/netgloo/configs/DatabaseConfig.java
@@ -17,17 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.driverClassName}")
-  private String DB_DRIVER;
-  
-  @Value("${db.password}")
-  private String DB_PASSWORD;
-  
-  @Value("${db.url}")
-  private String DB_URL;
-  
-  @Value("${db.username}")
-  private String DB_USERNAME;
+
 
   @Value("${spring.jpa.properties.hibernate.dialect}")
   private String HIBERNATE_DIALECT;
