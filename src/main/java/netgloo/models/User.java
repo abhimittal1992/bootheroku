967afe900b599a0133664badf0d7d37f3050d713
Star Mercy <abhimittal19@gmail.com>
2017-05-16 11:37:19 +0530

    ASdas

--- src/main/java/netgloo/models/User.java
+++ src/main/java/netgloo/models/User.java
@@ -4,12 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
-@Table(name="users")
 public class User {
 
   @Id
@@ -17,11 +14,9 @@ public class User {
   private long id;
   
   @NotNull
-  @Size(min = 3, max = 80)
   private String email;
   
   @NotNull
-  @Size(min = 2, max = 80)
   private String name;
 
   public User() { }
