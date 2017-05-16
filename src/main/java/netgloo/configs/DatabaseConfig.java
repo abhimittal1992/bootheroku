c96301c1892f95a9a3e51487057c5f005f0ad369
Star Mercy <abhimittal19@gmail.com>
2017-05-16 11:41:40 +0530

    aasd

--- src/main/java/netgloo/configs/DatabaseConfig.java
+++ src/main/java/netgloo/configs/DatabaseConfig.java
@@ -4,19 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class DatabaseConfig {
-
-  
-  @ConfigurationProperties(prefix = "spring.datasource")
-  public DataSource dataSource() {
-      return DataSourceBuilder.create().build();
-  }
-
-
-
-} // class DatabaseConfig
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
